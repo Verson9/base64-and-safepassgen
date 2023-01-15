5d2b573c1bb1bbcb7a23856b71224c0850151fb8
verson <przemyslaw.surma@outlook.com>
2023-01-15 23:22:15 +0100

    I spent ******* 3h trying to implement this base 64 gen and it is f*****up as hell... not going into cryptography ever again if possible...

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"encoding/base64"
 	"fmt"
 	"math/rand"
 	"os"
@@ -11,6 +10,7 @@ import (
 )
 
 const (
+	base64chars    = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 	capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	smallLetters   = "abcdefghijklmnopqrstuvwxyz"
 	numbers        = "0123456789"
@@ -19,14 +19,79 @@ const (
 	max            = 16
 )
 
+type Base64 struct {
+	encode    [64]byte
+	decodeMap [256]byte
+	strict    bool
+}
+
+func NewBase64() *Base64 {
+	e := new(Base64)
+	copy(e.encode[:], base64chars)
+
+	for i := 0; i < len(e.decodeMap); i++ {
+		e.decodeMap[i] = 0xFF
+	}
+	for i := 0; i < len(base64chars); i++ {
+		e.decodeMap[base64chars[i]] = byte(i)
+	}
+	return e
+}
+
+func (enc *Base64) encodeToBase64(dst, src []byte) {
+	if len(src) == 0 {
+		return
+	}
+	_ = enc.encode
+	di, si := 0, 0
+	n := (len(src) / 3) * 3
+	for si < n {
+		val := uint(src[si+0])<<16 | uint(src[si+1])<<8 | uint(src[si+2])
+
+		dst[di+0] = enc.encode[val>>18&0x3F]
+		dst[di+1] = enc.encode[val>>12&0x3F]
+		dst[di+2] = enc.encode[val>>6&0x3F]
+		dst[di+3] = enc.encode[val&0x3F]
+
+		si += 3
+		di += 4
+	}
+	remain := len(src) - si
+	if remain == 0 {
+		return
+	}
+	val := uint(src[si+0]) << 16
+	if remain == 2 {
+		val |= uint(src[si+1]) << 8
+	}
+	dst[di+0] = enc.encode[val>>18&0x3F]
+	dst[di+1] = enc.encode[val>>12&0x3F]
+	switch remain {
+	case 2:
+		dst[di+2] = enc.encode[val>>6&0x3F]
+		dst[di+3] = byte('=')
+	case 1:
+		dst[di+2] = byte('=')
+		dst[di+3] = byte('=')
+	}
+}
+
+func (enc *Base64) encodeString(src []byte) string {
+	buf := make([]byte, (len(src)+2)/3*4)
+	enc.encodeToBase64(buf, src)
+	return string(buf)
+}
+
 func base64gen(reader *bufio.Reader) {
 	fmt.Println("Type text to encode:")
 	readString, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println(err)
 	}
-	encoding := base64.StdEncoding.EncodeToString([]byte(readString[:len(readString)-1]))
-	fmt.Println(encoding)
+	strToEncode := readString[:len(readString)-1]
+	buf := make([]byte, (len(strToEncode)+2)/3*4)
+	NewBase64().encodeToBase64(buf, []byte(strToEncode))
+	fmt.Println(string(buf))
 	menu()
 }
 
@@ -96,6 +161,7 @@ Choose option by typing number:
 	}
 
 }
+
 func main() {
 	menu()
 }
